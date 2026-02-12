b609e5ddeb43e15ea02d3f297daafff34b7ccb70
Eljoker0880 <droid17934862@gmail.com>
2026-02-11 21:12:25 -0400

    LABEL

---
+++ Proyecto Label/Proyecto Label/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Proyecto_Label
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(50, 70);
+            label1.Name = "label1";
+            label1.Size = new Size(641, 23);
+            label1.TabIndex = 0;
+            label1.Text = "¡Bienvenido al formulario de Alan Batista LR-2025-02675!";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(737, 169);
+            Controls.Add(label1);
+            ForeColor = SystemColors.ButtonHighlight;
+            Name = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
---
+++ Proyecto Label/Proyecto Label/Form1.cs
@@ -0,0 +1,10 @@
+namespace Proyecto_Label
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
