51766422071a3c549f2db8a120db8c4a890e2d7e
Eljoker0880 <droid17934862@gmail.com>
2026-02-12 20:57:11 -0400

    combobox

--- opciones/opciones/Form1.Designer.cs
+++ opciones/opciones/Form1.Designer.cs
@@ -33,71 +33,102 @@
             radioButton3 = new RadioButton();
             button1 = new Button();
             label1 = new Label();
+            panel1 = new Panel();
+            label2 = new Label();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // radioButton1
             // 
             radioButton1.AutoSize = true;
-            radioButton1.Location = new Point(212, 127);
+            radioButton1.Location = new Point(267, 14);
             radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(94, 19);
+            radioButton1.Size = new Size(53, 19);
             radioButton1.TabIndex = 0;
             radioButton1.TabStop = true;
-            radioButton1.Text = "radioButton1";
+            radioButton1.Text = "Nada";
             radioButton1.UseVisualStyleBackColor = true;
             // 
             // radioButton2
             // 
             radioButton2.AutoSize = true;
-            radioButton2.Location = new Point(12, 127);
+            radioButton2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            radioButton2.Location = new Point(8, 14);
             radioButton2.Name = "radioButton2";
-            radioButton2.Size = new Size(94, 19);
+            radioButton2.Size = new Size(66, 19);
             radioButton2.TabIndex = 1;
             radioButton2.TabStop = true;
-            radioButton2.Text = "radioButton2";
+            radioButton2.Text = "Amigos";
             radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
             // 
             // radioButton3
             // 
             radioButton3.AutoSize = true;
-            radioButton3.Location = new Point(112, 127);
+            radioButton3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            radioButton3.Location = new Point(130, 14);
             radioButton3.Name = "radioButton3";
-            radioButton3.Size = new Size(94, 19);
+            radioButton3.Size = new Size(63, 19);
             radioButton3.TabIndex = 2;
             radioButton3.TabStop = true;
-            radioButton3.Text = "radioButton3";
+            radioButton3.Text = "Novios";
             radioButton3.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            button1.Location = new Point(59, 186);
+            button1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(73, 61);
             button1.Name = "button1";
             button1.Size = new Size(193, 62);
             button1.TabIndex = 3;
-            button1.Text = "button1";
+            button1.Text = "Seleccionar";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(133, 77);
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(90, 9);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(163, 32);
             label1.TabIndex = 4;
-            label1.Text = "label1";
+            label1.Text = "¿Que somos?";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(radioButton3);
+            panel1.Controls.Add(radioButton2);
+            panel1.Controls.Add(radioButton1);
+            panel1.Location = new Point(4, 113);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(342, 143);
+            panel1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(148, 56);
+            label2.Name = "label2";
+            label2.Size = new Size(35, 32);
+            label2.TabIndex = 6;
+            label2.Text = "...";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(350, 260);
+            Controls.Add(label2);
+            Controls.Add(panel1);
             Controls.Add(label1);
-            Controls.Add(button1);
-            Controls.Add(radioButton3);
-            Controls.Add(radioButton2);
-            Controls.Add(radioButton1);
             Name = "Form1";
             Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -109,5 +140,7 @@
         private RadioButton radioButton3;
         private Button button1;
         private Label label1;
+        private Panel panel1;
+        private Label label2;
     }
 }
--- opciones/opciones/Form1.cs
+++ opciones/opciones/Form1.cs
@@ -6,5 +6,29 @@ namespace opciones
         {
             InitializeComponent();
         }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                label2.Text = radioButton1.Text;
+            }
+            else if (radioButton2.Checked)
+            {
+                label2.Text = radioButton2.Text;
+            }
+            else if (radioButton3.Checked)
+            {
+                label2.Text = radioButton3.Text;
+
+                return;
+            }
+            }
+        }
     }
-}
+
