d82dab5b1ff13a7847fb982019256b47c2e3117b
Eljoker0880 <droid17934862@gmail.com>
2026-02-25 18:33:59 -0400

    trabajando

---
+++ Cronometro/Cronometro/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Cronometro
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            button2 = new Button();
+            label1 = new Label();
+            button1 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DodgerBlue;
+            button2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(12, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(43, 38);
+            button2.TabIndex = 1;
+            button2.Text = "+";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(44, 108);
+            label1.Name = "label1";
+            label1.Size = new Size(165, 86);
+            label1.TabIndex = 3;
+            label1.Text = "0:00";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 128, 0);
+            button1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(133, 255);
+            button1.Name = "button1";
+            button1.Size = new Size(109, 54);
+            button1.TabIndex = 0;
+            button1.Text = "PARAR";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.LimeGreen;
+            button3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(12, 255);
+            button3.Name = "button3";
+            button3.Size = new Size(115, 55);
+            button3.TabIndex = 4;
+            button3.Text = "INICIAR";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Red;
+            button4.Font = new Font("Segoe UI", 8.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.Location = new Point(87, 197);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 5;
+            button4.Text = "CANCELAR";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 64);
+            ClientSize = new Size(254, 316);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private Button button2;
+        private Label label1;
+        private Button button1;
+        private Button button3;
+        private Button button4;
+    }
+}
---
+++ Cronometro/Cronometro/Form1.cs
@@ -0,0 +1,93 @@
+﻿namespace Cronometro
+{
+    public partial class Form1 : Form
+    {
+        int tiempoRestante = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000; // 1 segundo exacto
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (tiempoRestante > 0)
+            {
+                tiempoRestante--;
+
+                int minutos = tiempoRestante / 60;
+                int segundos = tiempoRestante % 60;
+
+                label1.Text = minutos.ToString() + ":" + segundos.ToString("00");
+            }
+            else
+            {
+                timer1.Stop();
+                MessageBox.Show("Tiempo terminado ⏰");
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string input = Microsoft.VisualBasic.Interaction.InputBox(
+                "Ingrese el tiempo:",
+                "Configurar Cronómetro",
+                "60");
+
+            int tiempo;
+
+            if (int.TryParse(input, out tiempo))
+            {
+                DialogResult resultado = MessageBox.Show(
+                    "¿Desea ingresar el tiempo en minutos?",
+                    "Tipo de tiempo",
+                    MessageBoxButtons.YesNo);
+
+                if (resultado == DialogResult.Yes)
+                {
+                    tiempoRestante = tiempo * 60; // minutos a segundos
+                }
+                else
+                {
+                    tiempoRestante = tiempo; // segundos directos
+                }
+
+                label1.Text = tiempoRestante.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un número válido.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (tiempoRestante > 0)
+            {
+                timer1.Start();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();          // Detiene el cronómetro
+            tiempoRestante = 0;     // Pone el tiempo en 0
+            label1.Text = "0:00";   // Muestra 0:00 en pantalla
+        }
+    }
+}
