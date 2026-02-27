591953f932d9e5b935a31a3379186c3208d7234c
Eljoker0880 <droid17934862@gmail.com>
2026-02-27 18:35:28 -0400

    trabajando

--- Verif_Palindroma/Verif_Palindroma/Form1.Designer.cs
+++ Verif_Palindroma/Verif_Palindroma/Form1.Designer.cs
@@ -28,12 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(70, 187);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(223, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            textBox1.Enter += textBox1_TextChanged;
+            textBox1.KeyDown += textBox1_KeyDown;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(376, 29);
+            label1.TabIndex = 2;
+            label1.Text = "Verifiacar Palabras palindromas";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(70, 159);
+            label2.Name = "label2";
+            label2.Size = new Size(157, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Colocar palabra:";
+            label2.Click += label2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.Ivory;
+            ClientSize = new Size(390, 274);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
     }
 }
--- Verif_Palindroma/Verif_Palindroma/Form1.cs
+++ Verif_Palindroma/Verif_Palindroma/Form1.cs
@@ -6,5 +6,34 @@ namespace Verif_Palindroma
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string texto = textBox1.Text
+                    .ToLower()
+                    .Replace(" ", "")
+                    .Trim();
+
+                string invertido = new string(texto.Reverse().ToArray());
+
+                MessageBox.Show(texto == invertido
+                    ? "Es un palíndromo"
+                    : "No es un palíndromo");
+
+                e.SuppressKeyPress = true; // ?? evita que agregue salto de línea
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
